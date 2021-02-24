17b6d893ce6624ec06dd35b431623e416a25a4ec
bjungic <boki.kibo@gmail.com>
2021-02-24 16:54:24 +0100

    1

--- src/main/java/com/java/boot/billy/hnb/services/ReadHNB.java
+++ src/main/java/com/java/boot/billy/hnb/services/ReadHNB.java
@@ -8,14 +8,10 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.netty.http.client.HttpClient;
-import reactor.util.retry.Retry;
 
 import java.sql.Date;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
@@ -37,6 +33,68 @@ public class ReadHNB {
 
     @Async
     public void getCrurrency() {
+
+//        getDanPoDan();
+
+        getGodinaPoGodina();
+
+    }
+
+    private void getGodinaPoGodina() {
+
+        Currency[] currency = null;
+        String zadnji = currencyRepository.getLast();
+        int godina;
+        if (!(zadnji == null)) {
+            godina = LocalDate.parse(zadnji).getYear();
+        } else godina = LocalDate.now().getYear();
+
+        Long timer = Calendar.getInstance().getTimeInMillis();
+
+        do {
+            try {
+                System.out.println(godina);
+                currency = builder.codecs(configurer -> configurer
+                        .defaultCodecs()
+                        .maxInMemorySize(16 * 1024 * 1024))
+                        .build()
+                        .get()
+                        .uri("http://api.hnb.hr/tecajn/v2?datum-primjene-od=" + godina + "-01-01&datum-primjene-do=" + godina + "-12-31")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .retrieve()
+                        .bodyToMono(Currency[].class)
+                        .block();
+
+
+                for (Currency c : currency) {
+                    Currency curr = currencyRepository.getCurrency(c.getValuta(), c.getDatum_primjene());
+                    if (curr == null) {
+                        currencyRepository.save(c);
+                    } else {
+                        curr.setKupovni_tecaj(c.getKupovni_tecaj());
+                        curr.setSrednji_tecaj(c.getSrednji_tecaj());
+                        curr.setProdajni_tecaj(c.getProdajni_tecaj());
+                        currencyRepository.save(curr);
+                    }
+                }
+                if (Calendar.getInstance().getTimeInMillis() >= timer) {
+                    timer += 360000; //3600000 - 1h; 60000 1min;
+                    logger.info("Broj zapisa: " + currencyRepository.countAll());
+                }
+
+            } catch (Exception e) {
+                System.out.println("catch");
+                System.out.println(e.getMessage());
+                getGodinaPoGodina();
+            }
+            godina = godina - 1;
+
+        } while (currency.length > 0);
+
+    }
+
+    private void getDanPoDan() {
+
         Currency[] currency;
         SimpleDateFormat smf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar cal = Calendar.getInstance();
@@ -51,7 +109,6 @@ public class ReadHNB {
         }
         Long sada = cal.getTimeInMillis();
         Long timer = Calendar.getInstance().getTimeInMillis();
-//        Long sada = Long.parseLong("772927200000");
 
         int i = 0;
         do {
@@ -79,14 +136,12 @@ public class ReadHNB {
             }
             if (Calendar.getInstance().getTimeInMillis() >= timer) {
                 timer += 360000; //3600000 - 1h; 60000 1min;
-//                System.out.println("Broj zapisa: " + currencyRepository.countAll());
                 logger.info("Broj zapisa: " + currencyRepository.countAll());
             }
 
             sada = sada - 86400000;
             if (!(currency.length > 0)) {
                 try {
-//                    System.out.println(currencyRepository.getLast());
                     logger.error(currencyRepository.getLast());
                     TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
@@ -95,5 +150,6 @@ public class ReadHNB {
                 i++;
             }
         } while (i < 10);
+
     }
 }
